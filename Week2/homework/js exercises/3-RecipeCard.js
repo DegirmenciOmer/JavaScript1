d1c0ac37d060e1659cf327e19278861ec970705e
Omer Degirmenci <59254278+DegirmenciOmer@users.noreply.github.com>
2020-03-30 16:21:29 +0200

    CHANGES ADDED
    'ex 2 && 4 indent; 
    ex 3 declare EMPTY object and use for ...in loop; 
    ex 5: use if & else if & else statements

---
+++ Week2/homework/js exercises/3-RecipeCard.js
@@ -0,0 +1,17 @@
+//DECLARE A VARIABLE THAT HOLDS AN OBJECT (YOUR MEAL RECIPE)
+//GIVE THE OBJECT 3 PROPERTIES:
+//A TITLE(STRING), A SERVINGS(NUMBER), 
+//AND AN INGREDIENTS (ARRAY OF STRINGS) PROPERTY.
+
+const mySpecialRecipe = {}
+
+mySpecialRecipe.mealname = "Vla",
+mySpecialRecipe.serves = 3,
+mySpecialRecipe.ingredients = ["1 lt. milk", "200 gr. sugar", "1 banana", "20 gr. starch"]
+
+
+//LOG EACH PROPERTY OUT SEPARATELY, USING A LOOP(FOR, WHILE OR DO/WHILE)
+
+for(const attr in mySpecialRecipe) {
+	console.log(attr);
+}
