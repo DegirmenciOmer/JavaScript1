db45caf14c4ea8648231bf3e31ee8616dc1daae5
Omer Degirmenci <rumidegirmenci0@gmail.com>
2020-05-14 14:46:30 +0200

    modify changes requested

--- Week2/homework/js exercises/2-TheEvenOddReporter.js
+++ Week2/homework/js exercises/2-TheEvenOddReporter.js
@@ -15,5 +15,7 @@ for (let i = 0; i < 21; i++){
 	if (i % 2 !== 0) {
 		console.log("The number " + i + " is ODD!");
 	}
-	else {console.log("The number " + i + " is even!");}
+	else {
+		console.log("The number " + i + " is even!");
+	}
 }
